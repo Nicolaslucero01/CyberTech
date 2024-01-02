016c04d7f25466b8db2d121dc1facab4052ce947
Nicolas <nicolucerocipo10@gmail.com>
2024-01-02 20:42:04 -0300

    Cambio

--- src/Components/Presentation/Hero/Hero.jsx
+++ src/Components/Presentation/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Hero.module.css';
+import styles from './hero.module.css';
 import carouselOne from '../../../Assets/117.png'
 import carouselTwo from '../../../Assets/158.png'
 import HeroMarcas from '../../Container/HeroMarcas/HeroMarcas';
